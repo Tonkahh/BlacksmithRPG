35feae75de77015a44e3b4e920531596e406f422
Tonkah <james.vnrfs.alt@gmail.com>
2025-01-09 10:32:14 +0100

    New blocks

---
+++ src/main/java/org/opusstudios/mods/blacksmithrpg/block/TestBlock4.java
@@ -0,0 +1,31 @@
+package org.opusstudios.mods.blacksmithrpg.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.Nullable;
+
+public class TestBlock4 extends Block {
+    public TestBlock4() {
+        super(Properties.of());
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext ctx) {
+        // Unique shape for the second block
+        return Shapes.join(Block.box(0, 0, 0, 16, 16, 16), Block.box(2, 16, 2, 14, 32, 14), BooleanOp.OR);
+    }
+
+    @Override
+    public void setPlacedBy(Level level, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack stack) {
+        super.setPlacedBy(level, pos, state, placer, stack);
+    }
+}
--- src/main/java/org/opusstudios/mods/blacksmithrpg/registry/BSRPGBlock.java
+++ src/main/java/org/opusstudios/mods/blacksmithrpg/registry/BSRPGBlock.java
@@ -4,16 +4,38 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
-import org.opusstudios.mods.blacksmithrpg.block.TestBlock;
+import org.opusstudios.mods.blacksmithrpg.block.*;
 import org.opusstudios.mods.blacksmithrpg.registry.impl.BlockItemDeferredRegister;
 
 public class BSRPGBlock {
 
     public static final BlockItemDeferredRegister INSTANCE = new BlockItemDeferredRegister();
+
     public static final DeferredHolder<Block,TestBlock> TEST_BLOCK = INSTANCE.register(
             "test_block",
             TestBlock::new,
             BlockItem::new);
+
+    public static final DeferredHolder<Block, TestBlock2> TEST_BLOCK_2 = INSTANCE.register(
+            "test_block2",
+            TestBlock2::new,
+            BlockItem::new);
+
+    public static final DeferredHolder<Block, TestBlock3> TEST_BLOCK_3 = INSTANCE.register(
+            "test_block3",
+            TestBlock3::new,
+            BlockItem::new);
+
+    public static final DeferredHolder<Block, TestBlock4> TEST_BLOCK_4 = INSTANCE.register(
+            "test_block4",
+            TestBlock4::new,
+            BlockItem::new);
+
+    public static final DeferredHolder<Block, Hearth> HEARTH = INSTANCE.register(
+            "hearth",
+            Hearth::new,
+            BlockItem::new);
+
     public static void register(IEventBus bus) {
         INSTANCE.register(bus);
     }
